97547359097884156f5835cc6fa3459126efb4c7
johnn4127 <johnn4127@gmail.com>
2023-09-25 15:16:17 -0400

    adds foximg

--- forms/src/components/FoxImageFetcher.jsx
+++ forms/src/components/FoxImageFetcher.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+
 
 const FoxImageFetcher = () => {
+    const [foxImage, setFoxImage] = useState('')
+
+    const fetchFox = async() => {
+        //this is the api key where I will retrieve the data
+        let response = await fetch('https://randomfox.ca/floof/')
+        let data = await response.json()
+        setFoxImage(data.image)
+        console.log(data)
+    }
+
+    useEffect(() => {
+        fetchFox()
+    }, [])
+
+
   return (
-    <div>FoxImageFetcher</div>
+    <div>
+        
+        <img src={foxImage} alt="" />
+    </div>
   )
 }
 
