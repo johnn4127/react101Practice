aa5a46a58b279b1aafb3d13d06590441c6f3183a
johnn4127 <johnn4127@gmail.com>
2023-09-25 14:19:45 -0400

    adds foximagefetcher

---
+++ News/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import News from './components/News'
+import './App.css'
+const App = () => {
+  return (
+    <div>
+      <News/>
+    </div>
+  )
+}
+
+export default App
