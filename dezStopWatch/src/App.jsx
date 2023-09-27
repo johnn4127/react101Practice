b438a8a4f16a77c262f46d62eb9d99c28e155814
johnn4127 <johnn4127@gmail.com>
2023-09-27 16:47:12 -0400

    deletes unnecessary code

--- dezStopWatch/src/App.jsx
+++ dezStopWatch/src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 import DezStopWatch from './components/DezStopWatch'
-import Stopwatch from '../../stopwatch/src/components/stopwatch'
+
 function App() {
   
 
