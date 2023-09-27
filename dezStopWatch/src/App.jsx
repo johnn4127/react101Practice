e85329236d4c5ac531c8f1ab0cd5d7e76fc52cb7
johnn4127 <johnn4127@gmail.com>
2023-09-27 14:05:52 -0400

    adds Dez's way for stopwatch

---
+++ dezStopWatch/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import DezStopWatch from './components/DezStopWatch'
+import Stopwatch from '../../stopwatch/src/components/stopwatch'
+function App() {
+  
+
+  return (
+    <>
+      <DezStopWatch/>
+    </>
+  )
+}
+
+export default App
