1202bfc0bca01d440cef16aa32b5edbcbe57c381
johnn4127 <johnn4127@gmail.com>
2023-09-27 15:13:28 -0400

    adds update to stopwatch

--- dezStopWatch/src/components/DezStopWatch.jsx
+++ dezStopWatch/src/components/DezStopWatch.jsx
@@ -1,53 +1,57 @@
-import React,{useState,useEffect, useRef} from 'react'
-
+import React, { useState, useEffect, useRef } from 'react';
 
 const DezStopWatch = () => {
-    const [lapse, setLapse] = useState(0)
-    const [running, setRunning] = useState(false)
-    let intervalRef = useRef(null)
+  const [lapse, setLapse] = useState(0);
+  const [running, setRunning] = useState(false);
+  let intervalRef = useRef(null);
 
+  useEffect(() => {
+    return () => {
+      clearInterval(intervalRef.current);
+    };
+  }, []);
 
-    useEffect(() => {
-      //runs when the component mounts (aka on initialization)
+  const handleClearClick = () => {
+    clearInterval(intervalRef.current);
+    setLapse(0);
+    setRunning(false);
+  };
+
+  const convertToTime = (ms) => {
+    const hours = Math.floor(ms / 3600000);
+    const minutes = Math.floor((ms % 3600000) / 60000);
+    const seconds = Math.floor((ms % 60000) / 1000);
+    const milliseconds = ms % 1000;
     
+    return `${hours}:${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}.${milliseconds}`;
+  };
 
-        //the return below will run when the component unmounts
-      return () =>  {
-        clearInterval(intervalRef.current)
-      }
-    }, [])
-    
-    const handleClearClick = () =>{
-        //I want this to make the clock stop
-        clearInterval(intervalRef.current)
-        setLapse(0)
-        setRunning(false)
+  const handleRunClick = () => {
+    if (running) {
+      clearInterval(intervalRef.current);
+    } else {
+      let startTime = Date.now() - lapse;
+      intervalRef.current = setInterval(() => {
+        setLapse(Date.now() - startTime);
+      }, 0);
     }
+    setRunning(!running);
+  };
 
-    const handleRunClick = () => {
-        //Needs to check if the clock is running
-        if(running){
-            //stop the clock by allowing the user to pause
-            clearInterval(intervalRef.current)
-        }
-        else {
-            let startTime = Date.now() -lapse;
-            console.log('This is the start time', startTime)
-            
-            intervalRef.current = setInterval(()=>{
-                setLapse(Date.now()- startTime)
-            }, 0)
-        }
-        setRunning(!running)
-    }
   return (
     <div>
-        <label>{lapse}</label>
-        <button onClick={handleRunClick}>{running ? 'Stop' : 'Start'} </button>
-        <button onClick={handleClearClick}> Clear</button>
-
+      <div>
+        <h1>Stopwatch</h1>
+      </div>
+      <div className='clock'>
+        <label>{convertToTime(lapse)}</label>
+      </div>
+      <div className='stopWatchButtons'>
+        <button onClick={handleRunClick}>{running ? 'Stop' : 'Start'}</button>
+        <button onClick={handleClearClick}>Clear</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DezStopWatch
+export default DezStopWatch;
