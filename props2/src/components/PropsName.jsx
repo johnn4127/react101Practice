a0feb2fbca6fecec553625180f6737be27073aa9
John Nguyen <johnn4127@gmail.com>
2023-10-04 09:48:13 -0400

    useContext lesson. Go go go (#14)

---
+++ props2/src/components/PropsName.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const PropsName = (props) => {
+  return (
+    <div>
+        <h1>{props.nameaaa}</h1>
+        <p>{props.birthYear}</p>
+        <h2>{props.favColor}</h2>
+        
+    </div>
+  )
+}
+
+export default PropsName
