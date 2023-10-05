36502a6324802866daa4eb62f952344ec486f0cf
John Nguyen <johnn4127@gmail.com>
2023-10-05 10:11:37 -0400

    adds pointguard (#19)

--- useContext/src/components/Pointguard.jsx
+++ useContext/src/components/Pointguard.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {useState} from 'react'
 import { PlayerContext } from '../App'
 
@@ -9,10 +9,11 @@ const Pointguard = () => {
     const [courtVision, setCourtVision] = useState(85)
     const[turnover, setTurnover] = useState(0)
 
-
+    const team = useContext(PlayerContext)
   return (
     <div>
         <h2>Point Guard Stats</h2>
+        <p>Team: {team}</p>
         <p>Ability to see the court: {courtVision}</p>
         <p>Likelihood to turn the ball over: {turnover}</p>
     </div>
